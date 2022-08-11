abac0d7e38637d3e90a04a4a65fe633ad15ad323
haihung <hungnh12062000@gmail.com>
2022-08-12 00:24:32 +0700

    component

--- src/views/App.js
+++ src/views/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './Example/MyComponent';
+// import = require 
 
-function App() {
+// Arrow Function component
+const App = () => {
+// function App() { 
   return (
+
+    // jsx
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Downgrade React 18 to 17
         </p>
+        
+        <MyComponent/>
+
       </header>
     </div>
+
   );
 }
 

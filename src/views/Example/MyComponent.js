8e8b736480b20cb61702d3699bc7b9ab7371b52f
haihung <hungnh12062000@gmail.com>
2022-08-15 21:48:02 +0700

    handle dom event

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
-class MyComponent extends React.Component
-{
+class MyComponent extends React.Component {
+     // State là những data có thê bị thay đổi. Khi state thay đổi thì UI sẽ được render lại
+     state = {
+          name: 'Hai Hung',
+          age: '22'
+     }
+
+     // update state
+     hanleOnchange = (event) => {
+          this.setState({
+               name: event.target.value
+          })
+     }
+
+     //click button
+     handleClickButton = () => {
+          alert('Click me!');
+     }
 
      render() {
           return (
                //output
-               <div>Hello jsx</div>  
+               <>
+                    <div className="first">
+                         <input type="text" value={this.state.name} onChange={(event) => { this.hanleOnchange(event) }} />
+                         <div className="name">{this.state.name}</div>
+                    </div>
+                    <div className="second">
+                         <button onClick={() => this.handleClickButton()}>click me!</button>
+                    </div>
+               </>
           )
      }
 }
